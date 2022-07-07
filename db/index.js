7e7f7d781cb4fa299e3ae631396f53061eaed6e1
ericpkatz <ericpkatz@gmail.com>
2022-07-06 20:39:33 -0400

    top-ranked

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/the_acme_item_tracker_db');
 
-const { VIRTUAL, STRING, INTEGER } = Sequelize;
+const { STRING, INTEGER } = Sequelize;
 
 const User = conn.define('user', {
   name: {
@@ -12,12 +12,6 @@ const User = conn.define('user', {
 const Thing = conn.define('thing', {
   name: {
     type: STRING 
-  },
-  highRanked: {
-    type: VIRTUAL,
-    get: function(){
-      return this.ranking > 5;
-    }
   },
   ranking: {
     type: INTEGER,
