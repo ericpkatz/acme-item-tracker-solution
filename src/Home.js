0dd6a2f566dc9803f00f0d7195b04ba374e1e61d
ericpkatz <ericpkatz@gmail.com>
2022-07-08 20:42:49 -0400

    most proflific user

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Home = ({ users, things, topRanked })=> {
+const Home = ({ users, things, topRanked, max, usersWithMost })=> {
   return (
     <div>
       <h1>Home</h1>
@@ -20,6 +20,16 @@ const Home = ({ users, things, topRanked })=> {
           })
         }
       </ul>
+      <h2>Users With Most Things ({ max} )</h2>
+      <ul>
+      {
+        usersWithMost.map( user => {
+          return (
+            <li key={ user.id }>{ user.name }</li>
+          );
+        })
+      }
+      </ul>
     </div>
   );
 };
@@ -27,10 +37,38 @@ const Home = ({ users, things, topRanked })=> {
 const mapSToP = (s)=> {
   const topRank = Math.max(...s.things.map(thing => thing.ranking));
   const topRanked = s.things.filter(thing => thing.ranking === topRank);
+  //map of userId to count of their things
+  const userThingMap = s.things.reduce((acc, thing)=> {
+    if(thing.userId !== null){
+      acc[thing.userId] = acc[thing.userId] || [];
+      acc[thing.userId]++;
+    }
+    return acc;
+  }, {});
+  let max = 0;
+  const entries = Object.entries(userThingMap);
+  //find the max amount of things owned by any user
+  entries.forEach( entry => {
+    if(entry[1] >= max){
+      max = entry[1];
+    }
+  });
+  const usersWithMost = [];
+  //find the users who have the max things
+  entries.forEach( entry => {
+    //we want the user, not the userId (all keys are strings)
+    const user = s.users.find(user => user.id === entry[0]*1);
+    if(user){
+      usersWithMost.push(user);
+    }
+  });
+
   return {
+    max,
     users: s.users,
     things: s.things,
-    topRanked
+    topRanked,
+    usersWithMost
   };
 };
 
